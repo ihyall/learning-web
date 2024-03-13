285d0a9cc29d6a9de57af1dd963ae9fc666fbe36
ihyall <ihy_all@mail.ru>
2024-03-13 15:03:33 +0300

    graph vue project added

---
+++ vue-graph-component/src/components/Graph.vue
@@ -0,0 +1,28 @@
+<script setup>
+import * as d3 from "d3";
+import { onMounted } from "vue";
+
+async function buildGraphSpace() {
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    svg = d3.select("#graph-svg")
+        .style({"border-width": "4px"})
+
+    g_node = svg.append("g")
+        .attr("id", "g-node")
+
+    
+}
+
+
+onMounted(() => { buildGraphSpace() })
+</script>
+
+<template>
+    <div id="graph-space">
+        <svg id="graph-svg"></svg>
+    </div>
+</template>
